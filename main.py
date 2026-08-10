a5cd1a5c0dc7187ff8d9c5ce3b1e147aa6ab23ee
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-08-10 16:21:24 +0300

    Basic Setup has been Created

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ from groq import Groq
 load_dotenv()
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-""""""
 
+
+#####################################
+
+#  TOOLS 
+
+##########################################
+
+def get_weather(city:str):
+    return f"The Weather in {city} is 30 degree and sunny."
+
+def calculator(expression:str):
+    return str(eval(expression))
+
+def search_web(query:str):
+    return f"Search Results for: {query}"
 
