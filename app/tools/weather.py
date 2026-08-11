62e7f276146b2fc41113144c568e88e2a07db4f3
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-08-11 17:32:48 +0300

    Weather tool Created

---
+++ app/tools/weather.py
@@ -0,0 +1,12 @@
+def get_weather(city:str) -> str:
+    '''
+    Get Weather information for a city.
+
+    This is currently a mock Implementation.
+    A Real Weather Api key is Also Connected
+    
+    '''
+
+    return f"The Weather in the {city} is 50° and sunny."
+
+
