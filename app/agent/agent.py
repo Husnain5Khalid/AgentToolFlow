d0df418e182becdaa5ad874d56934e350e0e760f
Husnain5khalid <husnainkhalidkhan6@gmail.com>
2026-08-12 15:44:28 +0300

    Tell The Agent About Tools

--- app/agent/agent.py
+++ app/agent/agent.py
@@ -7,4 +7,57 @@ class Agent:
         self.client = Groq(api_key=GROQ_API_KEY)
         self.model = GROQ_MODEL
 
-        
+    def get_tools(self):
+        return [
+            {
+                "type": "function",
+                "function": {
+                    "name": "get_weather",
+                    "description": "Get the current weather for a city.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "city": {
+                                "type": "string",
+                                "description": "The name of the city."
+                            }
+                        },
+                        "required": ["city"]
+                    }
+                }
+            },
+            {
+                "type": "function",
+                "function": {
+                    "name": "calculate",
+                    "description": "Calculate a mathematical expression.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "expression": {
+                                "type": "string",
+                                "description": "The mathematical expression."
+                            }
+                        },
+                        "required": ["expression"]
+                    }
+                },
+            },
+            {
+                "type": "function",
+                "function": {
+                    "name": "search_web",
+                    "description": "Search the web for information.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "query": {
+                                "type": "string",
+                                "description": "The search query."
+                            }
+                        },
+                        "required": ["query"]
+                    }
+                }
+            }
+        ]
